7d143feda6656054bd90e68d4c79e9f576a479ed
Arash Akbarinia <akbarinia.arash@gmail.com>
2023-11-21 16:26:56 +0100

    first draft of sphinx and readthedocs documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src/'))
+
+import osculari
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Osculari'
+copyright = '2023, Arash Akbarinia'
+author = 'Arash Akbarinia'
+release = osculari.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_domain_indices = True
+html_baseurl = "https://osculari.readthedocs.io"
---
+++ docs/source/osculari.models.rst
@@ -0,0 +1,45 @@
+osculari.models
+=======================
+
+Submodules
+----------
+
+osculari.models.model\_utils module
+-----------------------------------
+
+.. automodule:: osculari.models.model_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+osculari.models.pretrained\_layers module
+-----------------------------------------
+
+.. automodule:: osculari.models.pretrained_layers
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+osculari.models.pretrained\_models module
+-----------------------------------------
+
+.. automodule:: osculari.models.pretrained_models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+osculari.models.readout module
+------------------------------
+
+.. automodule:: osculari.models.readout
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: osculari.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
