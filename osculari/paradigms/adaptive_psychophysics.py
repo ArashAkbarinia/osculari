9db2c650870cbcbf8a22e7da3e78d48a1dd01ee2
ArashAkbarinia <akbarinia.arash@gmail.com>
2023-12-11 23:02:42 +0100

    commenting the paradigms module

--- osculari/paradigms/adaptive_psychophysics.py
+++ osculari/paradigms/adaptive_psychophysics.py
@@ -22,32 +22,74 @@ def staircase(model: nn.Module,
               dataset_fun: Callable[[float], Tuple],
               low_val: float, high_val: float, device: Optional[torch.device] = None,
               max_attempts: Optional[int] = 20) -> npt.NDArray:
-    """Computing the psychometric function following staircase procedure."""
-    # device
+    """
+        Computes the psychometric function following the staircase procedure.
+
+        Args:
+            model: The neural network model to be evaluated.
+            test_fun: Function for evaluating the model. This function must accept three
+             positional arguments (i.e., model, db_loader, device). The output of this function
+             should be a dictionary containing the key `accuracy`.
+            dataset_fun: Function for creating the dataset and dataloader. This function must
+             accept one argument (mid_val, i.e., the current value to be tested). This funtion must
+             return a tuple of three elements (i.e., dataset, batch_size, threshold).
+            low_val: The lower bound of the stimulus range.
+            high_val: The upper bound of the stimulus range.
+            device: The device to which the model and data will be transferred
+             (default: CUDA if available) (optional).
+            max_attempts: The maximum number of attempts allowed in the staircase procedure.
+
+        Returns:
+            A NumPy array containing the psychometric function data points containing two columns,
+            the first column tested values and the second column obtained accuracies.
+        """
+
+    # Set the device
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Calculate the midpoint of the initial stimulus range
     mid_val = (low_val + high_val) / 2
+
+    # List to store the psychometric function data points
     results = []
+
+    # Number of attempts to perform the staircase procedure
     attempt_num = 0
+
+    # Perform the staircase procedure until convergence
     while True:
-        # creating the dataset and dataloader
+        # Create the dataset and dataloader for the current midpoint
         dataset, batch_size, th = dataset_fun(mid_val)
         db_loader = TorchDataLoader(
             dataset, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=True
         )
-        # making the test
+
+        # Evaluate the model on the current midpoint
         test_log = test_fun(model, db_loader, device)
         accuracy = np.mean(test_log['accuracy'])
+
+        # Check if accuracy is within the acceptable range
         if 1 < accuracy < 0:
             raise RuntimeError('Accuracy for staircase procedure must be between 0 and 1.')
+
+        # Append the current midpoint and accuracy to the results list
         results.append(np.array([mid_val, accuracy]))
+
+        # Calculate the new stimulus range for the next iteration
         new_low, new_mid, new_high = paradigm_utils.midpoint(
             accuracy, low_val, mid_val, high_val, th=th
         )
+
+        # Check if the procedure has converged or reached the maximum number of attempts
         if new_mid is None or attempt_num == max_attempts:
             break
         else:
+            # Update the stimulus range
             low_val, mid_val, high_val = new_low, new_mid, new_high
+
+        # Increment the attempt counter
         attempt_num += 1
+
+    # Return the psychometric function data points
     return np.array(results)
--- osculari/paradigms/forced_choice.py
+++ osculari/paradigms/forced_choice.py
@@ -9,57 +9,117 @@ import torch.nn as nn
 
 from . import paradigm_utils
 
+__all__ = [
+    'epoch_loop',
+    'test_dataset',
+    'predict_dataset'
+]
+
 
 def epoch_loop(model: nn.Module, dataset: Iterator, optimiser: Union[torch.optim.Optimizer, None],
                device: Optional[torch.device] = None,
                return_outputs: Optional[bool] = False) -> Dict:
-    # device
+    """
+    Executes an epoch of training or testing.
+
+    Args:
+        model: The neural network model to be trained or tested.
+        dataset: The data loader providing data batches for training or testing.
+        optimiser: The optimiser used for training (if None it switches to testing mode).
+        device: The device to which the model and data will be transferred
+         (default: CUDA if available) (optional).
+        return_outputs: Whether to return model's outputs.
+
+    Returns:
+        A dictionary containing 'accuracy' and 'loss'.
+        If `return_outputs` is True, it will also include the 'output' key, which contains the
+        model's outputs for each data batch.
+    """
+
+    # Set the device
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # usually the code for train/test has a large overlap.
+    # Determine whether the model is in training or evaluation mode
     is_train = False if optimiser is None else True
-    # model should be in train/eval model accordingly
+
+    # Ensure the model is in the appropriate mode (train or eval)
     model.train() if is_train else model.eval()
     model.to(device)
 
-    accuracies = []
-    losses = []
-    outputs = []
+    # Initialise variables to store epoch-level statistics
+    accuracies = []  # List of batch-wise accuracies
+    losses = []  # List of batch-wise losses
+    outputs = []  # List of batch-wise model outputs
+
+    # Iterate over data batches
     with torch.set_grad_enabled(is_train):
         for batch_ind, batch_data in enumerate(dataset):
-            # moving the image and GT to device
-            inputs = [img.to(device) for img in batch_data[:-1]]
-            target = batch_data[-1].to(device)
+            # Transfer data to the device
+            inputs = [img.to(device) for img in batch_data[:-1]]  # Input images
+            target = batch_data[-1].to(device)  # Ground-truth labels
 
-            # calling the network
+            # Forward pass the model
             output = model(*inputs)
+
+            # Store outputs if requested
             if return_outputs:
                 outputs.extend([out.detach().cpu().numpy() for out in output])
 
-            # computing the loss function
+            # Compute loss
             loss = model.loss_function(output, target)
+            # Append batch-wise losses
             losses.extend([loss.item() for _ in range(inputs[0].size(0))])
-            # computing the accuracy
+            # Compute accuracy
             accuracy = paradigm_utils.accuracy(output, target)
+            # Append batch-wise accuracies
             accuracies.extend([accuracy for _ in range(inputs[0].size(0))])
 
-            # compute gradient and do SGD step
+            # Gradient computation and optimisation (if training mode)
             if is_train:
                 optimiser.zero_grad()
                 loss.backward()
                 optimiser.step()
+
+    # Create a dictionary containing epoch-level statistics
     epoch_log = {'accuracy': accuracies, 'loss': losses}
+
+    # Add 'output' key if requested
     if return_outputs:
         epoch_log['output'] = outputs
+
     return epoch_log
 
 
 def test_dataset(model: nn.Module, dataset: Iterator,
                  device: Optional[torch.device] = None) -> Dict:
+    """
+    Executes an epoch of testing.
+
+    Args:
+        model: The neural network model to be trained or tested.
+        dataset: The data loader providing data batches for training or testing.
+        device: The device to which the model and data will be transferred
+         (default: CUDA if available) (optional).
+
+    Returns:
+        A dictionary containing 'accuracy' and 'loss'.
+    """
     return epoch_loop(model, dataset, optimiser=None, device=device)
 
 
 def predict_dataset(model: nn.Module, dataset: Iterator,
                     device: Optional[torch.device] = None) -> Dict:
+    """
+    Executes an epoch of prediction.
+
+    Args:
+        model: The neural network model to be trained or tested.
+        dataset: The data loader providing data batches for training or testing.
+        device: The device to which the model and data will be transferred
+         (default: CUDA if available) (optional).
+
+    Returns:
+        A dictionary containing 'accuracy', 'loss' and 'output' (model's outputs).
+    """
     return epoch_loop(model, dataset, optimiser=None, device=device, return_outputs=True)
