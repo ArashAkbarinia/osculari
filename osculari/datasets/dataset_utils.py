2602d21d0ed1a490c448d4708a3f8e515eb62b66
ArashAkbarinia <akbarinia.arash@gmail.com>
2023-12-12 13:06:01 +0100

    docstring for datasets module

--- osculari/datasets/dataset_utils.py
+++ osculari/datasets/dataset_utils.py
@@ -10,73 +10,188 @@ from typing import Any, Union, Tuple, Optional, Sequence, List
 
 def _unique_colours(num_colours: int, channels: Optional[int] = 3,
                     exclude: [Optional[List]] = None) -> List:
-    """Creating a set of unique colours"""
+    """
+    Create a set of unique colors with the specified number of colors and channels.
+
+    Parameters:
+        num_colours (int): Number of unique colors to generate.
+        channels (Optional[int]): Number of color channels in each color (default is 3).
+        exclude (Optional[List]): List of colors to exclude from the generated set (default is None).
+
+    Returns:
+        List: A list of unique colors, each represented as a sequence of RGB values.
+    """
+    # Initialize exclude list if not provided
     exclude = [] if exclude is None else exclude
     colours = []
+
+    # Generate unique colors
     for i in range(num_colours):
         while True:
+            # Generate a random color
             colour = random_colour(channels=channels)
+
+            # Check if the color is not in the generated list and not in the exclude list
             if colour not in colours and colour not in exclude:
                 colours.append(colour)
                 break
+
     return colours
 
 
 def _patch_img(img_size: Tuple[int, int], num_colours: int, num_patches: int,
                channels: Optional[int] = 3) -> npt.NDArray:
-    """Create an image of patched colours, similar to Mondrian images."""
+    """
+    Create an image of patched colours, similar to Mondrian images.
+
+    Parameters:
+        img_size (Tuple[int, int]): Size of the image in pixels (height, width).
+        num_colours (int): Number of unique colours to use in the image.
+        num_patches (int): Number of patches to divide the image into.
+        channels (Optional[int]): Number of color channels in the image (default is 3).
+
+    Returns:
+        npt.NDArray: The generated image as a NumPy array.
+    """
+    # Initialise an empty image with the specified size and channels
     img = np.zeros((*img_size, channels), dtype='uint8')
+
+    # Generate an array of unique colours
     colours = _unique_colours(num_colours, channels=channels)
+
+    # Calculate the number of rows and columns for each patch
     patch_rows = int(np.ceil(img_size[0] / num_patches))
     patch_cols = int(np.ceil(img_size[1] / num_patches))
-    colour_ind = 0
+
+    colour_ind = 0  # Index for cycling through the unique colours
+
+    # Loop through each patch and fill it with a unique colour
     for r_ind in range(num_patches):
         srow = r_ind * patch_rows
         erow = min(srow + patch_rows, img.shape[0])
+
         for c_ind in range(num_patches):
             scol = c_ind * patch_cols
             ecol = min(scol + patch_cols, img.shape[1])
+
+            # Assign the current patch to a unique colour
             img[srow:erow, scol:ecol] = colours[colour_ind]
+
+            # Move to the next colour
             colour_ind += 1
+
+            # If all colours have been used, shuffle and reset the index
             if colour_ind == num_colours:
                 random.shuffle(colours)
                 colour_ind = 0
+
     return img
 
 
 def random_colour(channels: Optional[int] = 3) -> Sequence:
-    """Generating a random colour."""
-    return [np.random.randint(0, 256) for _ in range(channels)]
+    """
+    Generate a random color represented as a sequence of RGB values.
+
+    Parameters:
+        channels (Optional[int]): Number of color channels in the generated color (default is 3).
+
+    Returns:
+        Sequence: A sequence representing a random color, with values in the range [0, 255].
+    """
+    # Generate random values for each color channel
+    colour = [np.random.randint(0, 256) for _ in range(channels)]
+
+    return colour
 
 
 def michelson_contrast(img: npt.NDArray, contrast: float) -> npt.NDArray:
-    """Adjusting the contrast of an image."""
+    """
+    Adjust the contrast of an image using the Michelson contrast formula.
+
+    Parameters:
+        img (npt.NDArray): Input image as a NumPy array.
+        contrast (float): Contrast adjustment factor. Should be in the range [0, 1].
+
+    Returns:
+        npt.NDArray: Image with adjusted contrast.
+    """
+    # Ensure that the contrast value is within the valid range
     assert 0 <= contrast <= 1
-    return ((1 - contrast) / 2.0 + np.multiply(img, contrast)).astype(img.dtype)
+
+    # Apply Michelson contrast formula to adjust image contrast
+    adjusted_img = ((1 - contrast) / 2.0 + np.multiply(img, contrast)).astype(img.dtype)
+
+    return adjusted_img
 
 
 def _uniform_img(img_size: Tuple[int, int], value: Union[Sequence, int],
                  channels: Optional[int] = 3) -> npt.NDArray:
-    """Creating an image with uniform colours."""
+    """
+    Create an image with uniform colors.
+
+    Parameters:
+        img_size (Tuple[int, int]): Size of the image in pixels (height, width).
+        value (Union[Sequence, int]): Color value for the uniform background.
+         It can be a single integer value or a sequence of values (e.g., RGB).
+        channels (Optional[int]): Number of color channels in the image (default is 3).
+
+    Returns:
+        npt.NDArray: The generated image as a NumPy array.
+    """
+    # Convert value to NumPy array if it is a list, tuple, or ndarray
     if type(value) in (list, tuple, np.ndarray):
         value = np.array(value)
+
+    # If value is a float, ensure it is in the range [0, 1] and convert to uint8
     if type(value) is float or (type(value) is np.ndarray and value.dtype == float):
         if np.max(value) > 1.0:
             raise RuntimeError('Uniform background image: float values must be between 0 to 1.')
         else:
             value *= 255
-    return np.zeros((*img_size, channels), dtype='uint8') + value
+
+    # Create an image filled with the specified uniform color value
+    uniform_img = np.zeros((*img_size, channels), dtype='uint8') + value
+
+    return uniform_img
 
 
 def repeat_channels(img: npt.NDArray, channels: int) -> npt.NDArray:
-    """Adding channel dimension and repeating the image."""
-    return np.repeat(img[:, :, np.newaxis], channels, axis=2)
+    """
+    Add channel dimension and repeat the image along the new dimension.
+
+    Parameters:
+        img (npt.NDArray): Input image as a NumPy array.
+        channels (int): Number of times to repeat the image along the new channel dimension.
+
+    Returns:
+        npt.NDArray: Image with added channel dimension and repeated along that dimension.
+    """
+    # Add a new channel dimension and repeat the image along the new dimension
+    repeated_img = np.repeat(img[:, :, np.newaxis], channels, axis=2)
+
+    return repeated_img
 
 
 def background_img(bg_type: Any, bg_size: Union[int, Tuple], im2double=True) -> npt.NDArray:
-    """Creating a background image."""
+    """
+    Create a background image based on the specified type and size.
+
+    Parameters:
+        bg_type (Any): Type of the background. It can be a string representing a predefined type
+         ('uniform_achromatic', 'uniform_colour', 'random_achromatic', 'random_colour', or
+         'patch_colour', 'patch_achromatic'), or a value (int, float, list, tuple, or ndarray)
+         representing a uniform background color.
+        bg_size (Union[int, Tuple]): Size of the background image in pixels (height, width).
+        im2double (bool): If True, normalize the pixel values to the range [0, 1] (default is True).
+
+    Returns:
+        npt.NDArray: The generated background image as a NumPy array.
+    """
+    # Ensure bg_size is in tuple format
     if type(bg_size) not in [tuple, list]:
         bg_size = (bg_size, bg_size)
+
+    # Handle predefined background types
     if type(bg_type) in [str, np.str_]:
         if bg_type == 'uniform_achromatic':
             bg_img = _uniform_img(bg_size, np.random.randint(0, 256, dtype='uint8'))
@@ -93,8 +208,11 @@ def background_img(bg_type: Any, bg_size: Union[int, Tuple], im2double=True) -> 
             bg_img = _patch_img(bg_size, num_colours, num_patches, channels)
         else:
             raise RuntimeError('Unsupported background type %s.' % bg_type)
+    # Handle user-specified background values
     elif type(bg_type) in (list, tuple, np.ndarray, int, float):
         bg_img = _uniform_img(bg_size, bg_type)
     else:
         raise RuntimeError('Unsupported background type %s.' % bg_type)
+
+    # Normalise pixel values to the range [0, 1] if im2double is True
     return bg_img.astype('float32') / 255 if im2double else bg_img
--- osculari/datasets/gratings.py
+++ osculari/datasets/gratings.py
@@ -18,29 +18,77 @@ __all__ = [
 
 def sinusoid_grating(img_size: int, amplitude: float, theta: float, phase: float,
                      spatial_frequency: int) -> npt.NDArray[float]:
-    """Generate Sinusoid gratings."""
+    """Generates sinusoidal grating stimuli.
+
+    Args:
+        img_size: The desired size of the grating image.
+        amplitude: The amplitude of the sinusoidal modulation.
+        theta: The orientation of the grating (float in radians).
+        phase: The phase offset of the sinusoidal modulation.
+        spatial_frequency: The spatial frequency of the grating (int cycles per image).
+
+    Returns:
+        The generated sinusoidal grating stimuli.
+    """
+
+    # Generate the grid coordinates
     radius = img_size // 2
     [x, y] = np.meshgrid(range(-radius, radius + 1), range(-radius, radius + 1))
+
+    # Compute the frequency and phase parameters
     omega = [np.cos(theta), np.sin(theta)]
     lambda_wave = (img_size * 0.5) / (np.pi * spatial_frequency)
+
+    # Calculate the sinusoidal modulation
     stimuli = amplitude * np.cos((omega[0] * x + omega[1] * y) / lambda_wave + phase)
-    # if the target size is even, the generated stimuli is 1 pixel larger.
+
+    # If the target size is even, the generated stimuli is 1 pixel larger.
     if np.mod(img_size, 2) == 0:
         stimuli = stimuli[:-1, :-1]
+
     return stimuli
 
 
 def gaussian_img(img_size: int, sigma: float) -> npt.NDArray[float]:
+    """Generates a Gaussian-filtered image.
+
+    Args:
+        img_size: The desired size of the Gaussian image (int).
+        sigma: The standard deviation of the Gaussian filter (float).
+
+    Returns:
+        The generated Gaussian-filtered image (np.ndarray).
+    """
+
+    # Generate the grid coordinates
     radius = img_size // 2
     [x, y] = np.meshgrid(range(-radius, radius + 1), range(-radius, radius + 1))
+
+    # Compute the Gaussian filter
     gauss2d = np.exp(-(np.power(x, 2) + np.power(y, 2)) / (2 * np.power(sigma, 2)))
+
+    # Make the size odd
     if np.mod(img_size, 2) == 0:
         gauss2d = gauss2d[:-1, :-1]
+
+    # Normalize the intensity values to [0, 1]
     gauss2d = gauss2d / np.max(gauss2d)
+
+    # Return the Gaussian-filtered image
     return gauss2d
 
 
 class GratingsDataset(TorchDataset):
+    """
+    A dataset class for generating and storing sinusoidal grating stimuli.
+
+    Args:
+        img_size: The desired size of the grating images (int).
+        spatial_frequencies: A list of spatial frequencies for the gratings (optional).
+        thetas: A list of orientations for the gratings (optional).
+        gaussian_sigma: The standard deviation of the Gaussian filter (optional).
+        transform: A transformation to be applied to the stimuli (optional).
+    """
 
     def __init__(self, img_size: int, spatial_frequencies: Optional[Sequence[int]] = None,
                  thetas: Optional[Sequence[int]] = None, gaussian_sigma: Optional[float] = None,
@@ -58,22 +106,47 @@ class GratingsDataset(TorchDataset):
         return len(self.thetas) * len(self.sfs)
 
     def make_grating(self, idx: int, amplitude: float, channels=3) -> npt.NDArray[float]:
+        """
+        Constructs a sinusoidal grating image.
+
+        Args:
+            idx: The index of the grating to be generated.
+            amplitude: The amplitude of the sinusoidal modulation (float).
+            channels: The number of output channels (int; optional).
+
+        Returns:
+            The generated sinusoidal grating image (np.ndarray).
+        """
+
         theta_ind, sf_ind = np.unravel_index(idx, (len(self.thetas), len(self.sfs)))
         theta = self.thetas[theta_ind]
         sf = self.sfs[sf_ind]
         phase = 0
         stimuli = sinusoid_grating(self.img_size, amplitude, theta, phase, sf)
-        # multiply by a Gaussian
+
+        # Apply Gaussian filtering if specified
         if self.gaussian_sigma is not None:
             gauss_img = gaussian_img(self.img_size, self.gaussian_sigma)
             stimuli *= gauss_img
-        # bringing the image in the range of 0-1
+
+        # Normalise the image intensity to [0, 1]
         stimuli = (stimuli + 1) / 2
-        # converting it to N channels
+
+        # Repeat the image to multiple channels (if requested)
         stimuli = dataset_utils.repeat_channels(stimuli, channels=channels)
         return stimuli
 
     def __getitem__(self, idx: int) -> Union[torch.Tensor, npt.NDArray]:
+        """
+        Retrieves a grating image from the dataset.
+
+        Args:
+            idx: The index of the grating to be retrieved.
+
+        Returns:
+            The retrieved grating image (torch.Tensor or np.ndarray) after applying the specified
+             transformation.
+        """
         stimuli = self.make_grating(idx, 1.0)
         if self.transform:
             stimuli = self.transform(stimuli)
